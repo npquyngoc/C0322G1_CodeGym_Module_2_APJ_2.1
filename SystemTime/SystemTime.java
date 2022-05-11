12d561bd3883d9ab673befef0d17bec2537dd419
unknown <np.quyngoc@gmail.com>
2022-05-11 16:46:00 +0700

    Created SystemTime.java to display current system time.

---
+++ SystemTime/SystemTime.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+
+public class SystemTime {
+
+    public static void main(String[] args) {
+
+        Date getSystemTime = new Date();
+        System.out.println("Current system time is: " + getSystemTime);
+
+    }
+
+}
